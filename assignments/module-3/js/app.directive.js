827259326fc88323c750c6727047d3013feaf99c
Konstantin Gogov <konstantin.gogov@gmail.com>
2022-07-04 22:11:39 +0300

    made improvements to module-3 assignment

--- assignments/module-3/js/app.directive.js
+++ assignments/module-3/js/app.directive.js
@@ -1,11 +1,22 @@
 ; (function () {
   function FoundItemsDirective() {
+    function FoundItemsController() {
+      var $ctrl = this;
+
+      $ctrl.removeItem = function(index) {
+        $ctrl.onBtnClick({index: index});
+      };
+    }
+
     var ddo = {
       restrict: 'E',
       templateUrl: '../module-3/templates/foundItems.html',
+      controller: FoundItemsController,
+      controllerAs: '$ctrl',
+      bindToController: true,
       scope: {
         foundMenuItems: '<',
-        removeItemAction: '&',
+        onBtnClick: '&',
         onBtnClickDesc: '@'
       }
     };
